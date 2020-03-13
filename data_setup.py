23d88c0acf07f7f8b5de163c3f0f24aa38da5324
Andy Liu <andyliu5898@gmail.com>
2020-03-13 03:45:45 -0700

    update

--- data_setup.py
+++ data_setup.py
@@ -12,16 +12,19 @@ load_dotenv(dotenv_path)
 
 #number of files you want to convert to .wav, this
 #also affects the number of spectrograms you create
-FILES_TO_GENERATE = os.environ.get("FILES_TO_GENERATE")
-SONG_DURATION = os.environ.get("SONG_DURATION")
-SPECTROGRAM_WIDTH = os.environ.get("SPECTROGRAM_WIDTH") #in inches
-SPECTROGRAM_HEIGHT = os.environ.get("SPECTROGRAM_HEIGHT") #in inches
-SPECTROGRAM_DPI = os.environ.get("SPECTROGRAM_DPI")
-SONG_SAMPLING_RATE = os.environ.get("SONG_SAMPLING_RATE")
+FILES_TO_GENERATE = int(os.environ.get("FILES_TO_GENERATE"))
+SONG_DURATION = float(os.environ.get("SONG_DURATION"))
+SPECTROGRAM_WIDTH = float(os.environ.get("SPECTROGRAM_WIDTH")) #in inches
+SPECTROGRAM_HEIGHT = float(os.environ.get("SPECTROGRAM_HEIGHT")) #in inches
+SPECTROGRAM_DPI = int(os.environ.get("SPECTROGRAM_DPI"))
+SONG_SAMPLING_RATE = int(os.environ.get("SONG_SAMPLING_RATE"))
 
 print("Beginning Setup")
-
-shutil.rmtree("./Data/")
+try:
+    shutil.rmtree("./Data/")
+except:
+    print("Data folder does not exist to delete")
+    
 conversion.mp3_Convert(FILES_TO_GENERATE)
 
 spect_create.Spectrogram_Create(SPECTROGRAM_WIDTH, SPECTROGRAM_HEIGHT,
