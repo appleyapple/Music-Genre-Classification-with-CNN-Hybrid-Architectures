edee7d9972c3587abd63b4ce78e40ecfdcba67ac
Andy Liu <andyliu5898@gmail.com>
2020-03-13 02:52:47 -0700

    incorperated environment variables

--- data_setup.py
+++ data_setup.py
@@ -5,15 +5,19 @@ from Data_Processing import conversion
 from Data_Processing import spect_create
 from Data_Processing import data_csv_gen
 from Data_Processing import utils
+from dotenv import load_dotenv
+
+dotenv_path = "./setup.env" #Setup environment file
+load_dotenv(dotenv_path)
 
 #number of files you want to convert to .wav, this
 #also affects the number of spectrograms you create
-FILES_TO_GENERATE = 50
-SONG_DURATION = 30
-SPECTROGRAM_WIDTH = 3 #in inches
-SPECTROGRAM_HEIGHT = 3 #in inches
-SPECTROGRAM_DPI = 500
-SONG_SAMPLING_RATE = 16000
+FILES_TO_GENERATE = os.environ.get("FILES_TO_GENERATE")
+SONG_DURATION = os.environ.get("SONG_DURATION")
+SPECTROGRAM_WIDTH = os.environ.get("SPECTROGRAM_WIDTH") #in inches
+SPECTROGRAM_HEIGHT = os.environ.get("SPECTROGRAM_HEIGHT") #in inches
+SPECTROGRAM_DPI = os.environ.get("SPECTROGRAM_DPI")
+SONG_SAMPLING_RATE = os.environ.get("SONG_SAMPLING_RATE")
 
 print("Beginning Setup")
 
